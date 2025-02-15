d721290e5db37810c4fdc878f608f51e71d22066
craigkibera <craigituro@gmail.com>
2025-02-15 13:58:34 +0300

    init

--- lib/dog.py
+++ lib/dog.py
@@ -2,3 +2,11 @@
 
 class Dog:
     pass
+    def __init__(self,name,breed = "Mutt"):
+        self.name = name
+        self.breed = breed
+        
+    
+champ = Dog("champ")
+print (champ.name)
+print(champ.breed)
